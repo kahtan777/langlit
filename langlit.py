2dc83ab6dd556c894d358eff3b646c86dcfc12fb
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-09-09 19:17:40 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -26,8 +26,8 @@ data = loader.load()
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter = RecursiveCharacterTextSplitter(
     # Set a really small chunk size, just to show.
-    chunk_size = 10000,
-    chunk_overlap  = 500,
+    chunk_size = 1000,
+    chunk_overlap  = 100,
     length_function = len,
     add_start_index = True,
 )
