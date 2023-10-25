7b27b5e3fcb44f2716beba221e3ecc355829ceee
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-25 18:54:37 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -43,51 +43,51 @@ with st.container():
 
 
 
-    prompt = st.chat_input("Say something")
-    if prompt:
-        st.write(f"User has sent the following prompt: {prompt}")
-        print(prompt)
-        prompt = str(prompt)
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+    print(prompt)
+    prompt = str(prompt)
 
 
 
 
-    from langchain.document_loaders import WebBaseLoader
-    loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
-    data = loader.load()
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-    text_splitter = RecursiveCharacterTextSplitter(
-        # Set a really small chunk size, just to show.
-        chunk_size = 1000,
-        chunk_overlap  = 100,
-        length_function = len,
-        add_start_index = True,
-    )
-    texts = text_splitter.split_documents(data)
-    from langchain.vectorstores import Pinecone
-    from langchain.embeddings.openai import OpenAIEmbeddings
-    embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
-    pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
-    index_name = pinecone.Index('index-1')
-    from langchain.chat_models import ChatOpenAI
-    llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
-    llm.predict(str(prompt))
-    
-    
-    from langchain.chains import RetrievalQA
-    vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
-    retriever = vectordb.as_retriever()
-    from langchain.memory import ConversationBufferMemory
-    from langchain.chains import ConversationalRetrievalChain
-    
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages= True)
-    chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory= memory)
-    query = str(prompt)
-    Answer=chain.run({'question': query})
-    
-    
-    
-    with st.chat_message("user"):
-        st.write(str(Answer))
+from langchain.document_loaders import WebBaseLoader
+loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
+data = loader.load()
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size = 1000,
+    chunk_overlap  = 100,
+    length_function = len,
+    add_start_index = True,
+)
+texts = text_splitter.split_documents(data)
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
+pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
+index_name = pinecone.Index('index-1')
+from langchain.chat_models import ChatOpenAI
+llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
+llm.predict(str(prompt))
+
+
+from langchain.chains import RetrievalQA
+vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
+retriever = vectordb.as_retriever()
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages= True)
+chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory= memory)
+query = str(prompt)
+Answer=chain.run({'question': query})
+
+
+
+with st.chat_message("user"):
+    st.write(str(Answer))
     
 
