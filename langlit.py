9ec5049cbda809efae867787d7201cf65398d3a4
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-25 18:17:17 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -7,35 +7,35 @@ import streamlit as st
 
 
 st.set_page_config(layout="centered")
-with st.container():
-    video_html = """
-		<style>
 
-		#myVideo {
-		  position: fixed;
-		  left: 0;
-		  top: 70;
-		  width: 100%; 
-		  height: 70%;
-		}
+video_html = """
+<style>
+.video-container {
+  width: 100%; 
+  height: auto;
+}
 
-		.content {
-		  position: fixed;
-		  bottom: 0;
-		  background: rgba(0, 0, 0, 0.5);
-		  color: #f1f1f1;
-		  width: 100%;
-		  padding: 20px;
-		}
+video {
+  width: 100%; 
+  height: auto;
+}
 
-		</style>	
-		<video autoplay muted loop id="myVideo">
-		  <source src="https://static.streamlit.io/examples/star.mp4")>
-		  Your browser does not support HTML5 video.
-		</video>
-        """
+.content {
+  background: rgba(0, 0, 0, 0.5);
+  color: #f1f1f1;
+  width: 100%;
+  padding: 20px;
+}
+</style>	
+<div class="video-container">
+  <video autoplay muted loop id="myVideo">
+    <source src="https://static.streamlit.io/examples/star.mp4">
+    Your browser does not support HTML5 video.
+  </video>
+</div>
+"""
 
-    st.markdown(video_html, unsafe_allow_html=True)
+st.markdown(video_html, unsafe_allow_html=True)
 
 API_KEY=st.secrets["openAI_key"]
 P_API_KEY =st.secrets["pincone_key"]
