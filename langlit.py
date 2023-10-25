4c00ba15250c46994a9f1759d5baa0f46d04331f
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-25 18:57:12 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -49,8 +49,14 @@ if prompt:
     print(prompt)
     prompt = str(prompt)
 
+tab1, tab2 = st.tabs(["Cat", "Dog"])
 
-
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
 from langchain.document_loaders import WebBaseLoader
 loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
