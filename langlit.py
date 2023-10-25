40aa53addd1e3d88c68015254a9a6e467031270e
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-25 20:28:59 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -94,8 +94,9 @@ with st.chat_message("assistant"):
 # Initialize an empty dictionary to store key-value pairs
 data_dict = {}
 with st.sidebar:
-    key = prompt
-    value = Answer
+    while true :
+        key = prompt
+        value = Answer
     # Add the key-value pair to the dictionary
     data_dict[key] = value
     
