6ead136f057445baf9f7a3dc7f70192d5e16ec3d
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 12:59:05 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -96,7 +96,7 @@ Answer=chain.run({'question': query})
 chat_history.append(("Assistant", str(Answer))
 
 
-for speaker, message in chat_history :
+for speaker, message in chat_history 
     st.chat_message(message, speaker=speaker))
         
 
