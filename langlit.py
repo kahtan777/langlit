ad33901f47f6eaf1648478e6f8bbda2e36c8b739
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-27 00:11:08 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -77,8 +77,7 @@ retriever = vectordb.as_retriever()
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages= True)
 chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory= memory)
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
