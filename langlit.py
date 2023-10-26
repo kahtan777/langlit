4b133a07811cb6059a9a3c6eefa151408fa6fdfe
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 12:50:16 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -47,59 +47,50 @@ with st.container():
     st.markdown(video_html, unsafe_allow_html=True) 
     
 
-
-chat_history = []
 prompt = st.chat_input("Say something")
 if prompt:
     with st.chat_message("user"):
         st.write(str(prompt))
-    chat_history.append(("User", prompt))
 
+
+        
        
 
-    loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
-    data = loader.load()
-    
-    text_splitter = RecursiveCharacterTextSplitter(
-        # Set a really small chunk size, just to show.
-        chunk_size = 1000,
-        chunk_overlap  = 100,
-        length_function = len,
-        add_start_index = True,
-    )
-    texts = text_splitter.split_documents(data)
-    
-    embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
-    pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
-    index_name = pinecone.Index('index-1')
-    
-    llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
-    llm.predict(str(prompt))
-    
-    
-    
-    vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
-    retriever = vectordb.as_retriever()
-    
-    
-    # Create a memory to store the conversation history
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    
-    # Create the Conversational Retrieval Chain
-    chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
-    
-    # Run the conversation
-    Answer = chain.run({'question': prompt})
-    chat_history.append(("Assistant", Answer))
+loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
+data = loader.load()
 
-# Display the assistant's answer
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size = 1000,
+    chunk_overlap  = 100,
+    length_function = len,
+    add_start_index = True,
+)
+texts = text_splitter.split_documents(data)
+
+embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
+pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
+index_name = pinecone.Index('index-1')
+
+llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
+llm.predict(str(prompt))
+
+
+
+vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
+retriever = vectordb.as_retriever()
+
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages= True)
+chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory= memory)
+query = str(prompt)
+Answer=chain.run({'question': query})
+
+
 with st.chat_message("assistant"):
-    for speaker, message in chat_history:
-        st.write(f"{speaker}: {message}")
+    st.write(str(Answer))
         
 
-
-st.button("Voice input")
 
 
 
