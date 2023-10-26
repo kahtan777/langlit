8d54a4e128297d19c3126d04884845b8e3da62ab
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 19:03:06 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -105,12 +105,4 @@ st.button("Voice input")
 
 
 
-if prompt := st.chat_input():
 
-    openai.api_key = API_KEY
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
