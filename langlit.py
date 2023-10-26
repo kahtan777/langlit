40f8939b7faafbf41b85a4ffadbc0e50d588524f
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 20:27:41 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -91,7 +91,6 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     Answer=chain.run({'question': query})
