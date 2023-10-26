1f34e49b118f0a06e36d622fb31342d139aeefb9
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 12:19:29 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -3,6 +3,14 @@ import pandas as pd
 import plotly.express as px
 import os
 import pinecone
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
 
 API_KEY=st.secrets["openAI_key"]
 P_API_KEY =st.secrets["pincone_key"]
@@ -47,10 +55,10 @@ if prompt:
 
         
        
-from langchain.document_loaders import WebBaseLoader
+
 loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
 data = loader.load()
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 text_splitter = RecursiveCharacterTextSplitter(
     # Set a really small chunk size, just to show.
     chunk_size = 1000,
@@ -59,21 +67,19 @@ text_splitter = RecursiveCharacterTextSplitter(
     add_start_index = True,
 )
 texts = text_splitter.split_documents(data)
-from langchain.vectorstores import Pinecone
-from langchain.embeddings.openai import OpenAIEmbeddings
+
 embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
 pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
 index_name = pinecone.Index('index-1')
-from langchain.chat_models import ChatOpenAI
+
 llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
 llm.predict(str(prompt))
 
 
-from langchain.chains import RetrievalQA
+
 vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
 retriever = vectordb.as_retriever()
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+
 
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages= True)
 chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory= memory)
