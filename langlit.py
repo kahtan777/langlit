82d33c2e410f6ba82598a0ed6f44a32f2dc42bc2
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 13:16:03 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -55,49 +55,52 @@ chat_history = ["how can i help"]
     
 prompt = st.chat_input("Say something")
 
+
+
+    
+loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size = 1000,
+    chunk_overlap  = 100,
+    length_function = len,
+    add_start_index = True,
+)
+texts = text_splitter.split_documents(data)
+
+embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
+pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
+index_name = pinecone.Index('index-1')
+
+llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
+llm.predict(str(prompt))
+
+
+
+vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
+retriever = vectordb.as_retriever()
+
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages= True)
+chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory= memory)
+
 if prompt:
     # Append the user's prompt to the chat history
-    chat_history.append(("User", str(prompt)))
+    chat_history.append(("User", str(prompt))
 
-
-        
-    loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
-    data = loader.load()
-    
-    text_splitter = RecursiveCharacterTextSplitter(
-        # Set a really small chunk size, just to show.
-        chunk_size = 1000,
-        chunk_overlap  = 100,
-        length_function = len,
-        add_start_index = True,
-    )
-    texts = text_splitter.split_documents(data)
-    
-    embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
-    pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
-    index_name = pinecone.Index('index-1')
-    
-    llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
-    llm.predict(str(prompt))
-    
-    
-    
-    vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
-    retriever = vectordb.as_retriever()
-    
-    
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages= True)
-    chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory= memory)
+    # Run the conversation
     query = str(prompt)
-    Answer=chain.run({'question': query})
-    
+    Answer = chain.run({'question': query})
+
+    # Append the assistant's answer to the chat history
     chat_history.append(("Assistant", str(Answer))
 
+# Display the entire chat history
+for speaker, message in chat_history:
+    st.chat_message(message, speaker=speaker)
 
-    for speaker, message in chat_history:
-        with st.chat_message("assistant"):
-            st.write(message, speaker=speaker)
-            
 
         
 
