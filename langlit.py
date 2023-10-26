79b5ca164bd3fdad24233d962eaea4aea1a933a6
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 19:35:42 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -18,27 +18,8 @@ st.set_page_config(layout="centered")
 
 API_KEY = st.secrets["openAI_key"]
 P_API_KEY = st.secrets["pincone_key"]
+prompt = st.chat_input("Say something")
 
-loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
-data = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-    chunk_size=1000,
-    chunk_overlap=100,
-    length_function=len,
-    add_start_index=True,
-)
-texts = text_splitter.split_documents(data)
-
-embeddings = OpenAIEmbeddings(openai_api_key=API_KEY)
-pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
-index_name = pinecone.Index('index-1')
-llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0, openai_api_key=API_KEY )
-vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
-retriever = vectordb.as_retriever()
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
 
 with st.container():
     video_html = """
@@ -67,6 +48,27 @@ with st.container():
     </video>
     </div>
     """
+    loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size=1000,
+    chunk_overlap=100,
+    length_function=len,
+    add_start_index=True,
+)
+texts = text_splitter.split_documents(data)
+
+embeddings = OpenAIEmbeddings(openai_api_key=API_KEY)
+pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
+index_name = pinecone.Index('index-1')
+llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
+llm.predict(str(prompt))
+vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
+retriever = vectordb.as_retriever()
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
     st.markdown(video_html, unsafe_allow_html=True)
 
 if "messages" not in st.session_state:
