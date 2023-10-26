b817f28dad725ff3d0d3b31594a72de423898ebb
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-27 00:06:52 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -80,8 +80,7 @@ chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory=
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+
     
     
 if prompt := st.chat_input():
