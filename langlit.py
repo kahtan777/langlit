b41dfdb17b28e24ffb1f2f88e2833477e17c62f8
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-26 13:42:26 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -15,6 +15,24 @@ from langchain.chains import RetrievalQA
 API_KEY=st.secrets["openAI_key"]
 P_API_KEY =st.secrets["pincone_key"]
 
+
+loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size = 1000,
+    chunk_overlap  = 100,
+    length_function = len,
+    add_start_index = True,
+)
+texts = text_splitter.split_documents(data)
+
+embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
+pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
+index_name = pinecone.Index('index-1')
+
+
 st.set_page_config(layout="centered")
 
 with st.container():
@@ -55,24 +73,6 @@ if prompt:
 
 
         
-    
-
-loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
-data = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-    chunk_size = 1000,
-    chunk_overlap  = 100,
-    length_function = len,
-    add_start_index = True,
-)
-texts = text_splitter.split_documents(data)
-
-embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
-pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
-index_name = pinecone.Index('index-1')
-
 llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
 llm.predict(str(prompt))
 
