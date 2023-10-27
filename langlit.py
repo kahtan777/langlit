23e5dfb5e82c448d6a9dd60e2a5a2b41a9f624cf
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-27 19:51:59 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -10,19 +10,18 @@ from langchain.prompts import (
 import streamlit as st
 from streamlit_chat import message
 from utils import *
+keyy=st.secrets["openAI_key"]
 
-keyy = st.secrets["openAI_key"]
 
-# Video HTML
+
 video_html = """
 <style>
 .video-container {
     position: fixed; /* Fixed position to keep it in the top left corner */
     top: 0;
     left: 0;
-    width: 50%; /* Adjust the width to your desired size */
+    width: 25%; /* Adjust the width to your desired size */
     height: auto;
-    z-index: -1; /* Put the video behind the content */
 }
 
 video {
@@ -33,10 +32,8 @@ video {
 .content {
     background: rgba(0, 0, 0, 0.5);
     color: #f1f1f1;
-    width: 50%;
+    width: 100%;
     padding: 20px;
-    margin-left: 50%; /* Move the content to the right of the video */
-    position: relative; /* To position it relative to the video container */
 }
 </style>    
 <div class="video-container">
@@ -49,6 +46,10 @@ video {
 
 st.markdown(video_html, unsafe_allow_html=True)
 
+
+
+
+
 st.subheader("Chatbot with Langchain, ChatGPT, Pinecone, and Streamlit")
 
 if 'responses' not in st.session_state:
@@ -60,40 +61,46 @@ if 'requests' not in st.session_state:
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=keyy)
 
 if 'buffer_memory' not in st.session_state:
-    st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3, return_messages=True)
+            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
 
-system_msg_template = SystemMessagePromptTemplate.from_template(
-    template="""Answer the question as truthfully as possible using the provided context in Arabic only, 
-and if the answer is not contained within the text below, say 'I don't know'"""
-)
 
-human_msg_template = HumanMessagePromptTemplate.from_template("{input}")
+system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context in Arabic only, 
+and if the answer is not contained within the text below, say 'I don't know'""")
+
+
+human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 
 prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
 
 conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
 
-# Container for chat history
+
+
+
+# container for chat history
 response_container = st.container()
-# Container for text box
+# container for text box
 textcontainer = st.container()
+
 
 with textcontainer:
     query = st.text_input("Query: ", key="input")
     if query:
         with st.spinner("typing..."):
             conversation_string = get_conversation_string()
+            # st.code(conversation_string)
             refined_query = query_refiner(conversation_string, query)
             st.subheader("Refined Query:")
             st.write(refined_query)
             context = find_match(refined_query)
+            # print(context)  
             response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
         st.session_state.requests.append(query)
-        st.session_state.responses.append(response)
-
+        st.session_state.responses.append(response) 
 with response_container:
     if st.session_state['responses']:
+
         for i in range(len(st.session_state['responses'])):
-            message(st.session_state['responses'][i], key=str(i))
+            message(st.session_state['responses'][i],key=str(i))
             if i < len(st.session_state['requests']):
-                message(st.session_state["requests"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
