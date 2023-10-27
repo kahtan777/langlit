42b1c9a89439997271cf68f4b6f0a6243229449c
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-27 21:46:48 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -11,7 +11,7 @@ import streamlit as st
 from streamlit_chat import message
 from utils import *
 keyy=st.secrets["openAI_key"]
-left_column, col,right_column = st.columns([1,1, 3])
+left_column, right_column = st.columns([1, 3])
 
 with left_column:
     video_html = """
@@ -49,8 +49,7 @@ with left_column:
     st.markdown(video_html, unsafe_allow_html=True)
 
 
-with left_column:
-    st.write("|")
+
 
 
 
