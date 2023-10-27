a1a7070a8404ae825bc8189563888d937df7052a
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-27 11:22:59 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.express as px
 import os
 import pinecone
-import openai
 from langchain.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Pinecone
@@ -48,45 +47,44 @@ with st.container():
     st.markdown(video_html, unsafe_allow_html=True) 
     
 
+    prompt = st.chat_input("Say something")
+    if prompt:
+        with st.chat_message("user"):
+            st.write(str(prompt))
 
 
+            
         
-    
 
-loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
-data = loader.load()
+    loader = WebBaseLoader("https://medium.com/swlh/an-ultimate-guide-to-creating-a-startup-3b310f41d7e7")
+    data = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-    chunk_size = 100,
-    chunk_overlap  = 20,
-    length_function = len,
-    add_start_index = True,
-)
-texts = text_splitter.split_documents(data)
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size = 1000,
+        chunk_overlap  = 100,
+        length_function = len,
+        add_start_index = True,
+    )
+    texts = text_splitter.split_documents(data)
 
-embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
-pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
-index_name = pinecone.Index('index-1')
+    embeddings = OpenAIEmbeddings(openai_api_key =API_KEY) # set openai_api_key = 'your_openai_api_key' # type: ignore
+    pinecone.init(api_key=P_API_KEY, environment="gcp-starter")
+    index_name = pinecone.Index('index-1')
 
-llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
-
-
+    llm = ChatOpenAI(model_name='gpt-3.5-turbo-0301', temperature=0,openai_api_key =API_KEY ) # type: ignore
+    llm.predict(str(prompt))
 
 
-vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
-retriever = vectordb.as_retriever()
+
+    vectordb = Pinecone.from_documents(texts, embeddings, index_name='index-1')
+    retriever = vectordb.as_retriever()
 
 
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages= True)
-chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory= memory)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages= True)
+    chain = ConversationalRetrievalChain.from_llm(llm, retriever= retriever, memory= memory)
+    Answer=chain.run({'question': prompt})
 
 
-prompt = st.chat_input("Say something")
-if prompt:
-    # Split your prompt into multiple messages if it's too long
-    messages = [{"role": "user", "content": prompt}]
-    for msg in messages:
-        Answer = chain.run("question": msg)
-        with st.chat_message("assistant"):
-            st.write(str(Answer))
+    with st.chat_message("assistant"):
+        st.write(str(Answer))
