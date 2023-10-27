01982d0a4c447505e4194ed413029550b8115e20
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-27 10:39:23 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -89,9 +89,9 @@ if prompt := st.chat_input():
     Answer=chain.run({'question': prompt})
     openai.api_key = API_KEY
     st.session_state.messages.append({"role": 'assistant', 'content': Answer })
+    st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages, max_tokens=9000)
-    msg = response.choices[0].message
+    msg = Answer.message
     st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+    st.chat_message("assistant").write(msg)
     
