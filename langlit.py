aa5a1a97b6da0f986865bac901aa61453c85fec6
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-27 22:30:56 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -11,7 +11,7 @@ import streamlit as st
 from streamlit_chat import message
 from utils import *
 keyy=st.secrets["openAI_key"]
-left_column, right_column = st.columns([3,3])
+left_column, right_column = st.columns([7,3])
 
 with left_column:
     video_html = """
