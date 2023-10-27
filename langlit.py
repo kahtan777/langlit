6f4507061aa25cf07bb4ba06d8e0e6d75c54c230
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-27 11:00:06 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -89,10 +89,6 @@ if prompt := st.chat_input():
     openai.api_key = API_KEY
     st.session_state.messages.append({"role": 'assistant', 'content': Answer })
     st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    msg = Answer
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg)
 
 for msg in st.session_state.messages:
     st.chat_message("assistant").write(Answer)
