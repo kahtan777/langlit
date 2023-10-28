5ae42b25715c4ced8a2ac1e0844a983f88bd01b9
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-28 20:15:25 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -19,48 +19,60 @@ st.set_page_config(layout="wide")
 
 left_column, right_column = st.columns([7,3])
 
-with left_column:
-    video_html = """
-    <style>
+import streamlit as st
+
+video_html = """
+<style>
+.video-container {
+    width: 60%;
+    height: auto;
+    overflow: hidden;
+    border-radius: 0;
+    position: fixed;
+    top: 5%;
+    left: 3%;
+    z-index: 999; /* Ensure the video appears above other content */
+}
+
+@media (max-width: 768px) {
     .video-container {
-        width: 60%;
-        height: auto;
-        overflow: hidden;
-        border-radius: 0;
         position: fixed;
-        top: 5%;
-        left: 3%;
-        z-index: 999; /* Ensure the video appears above other content */
-    }
-
-    @media (max-width: 768px) {
-        .video-container {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-        }
-    }
-
-    video {
+        top: 0;
+        left: 0;
         width: 100%;
-        height: auto;
     }
+}
 
-    .content {
-        background: rgba(0, 0, 0, 0.5);
-        color: #f1f1f1;
-        width: 100%;
-        padding: 60px;
-    }
-    </style>    
-    <div class="video-container floating">
-    <video autoplay muted loop id="myVideo">
-        <source src="https://futurelaby.com/avatar/2023-10-28%2014-19-34.mp4">
-        Your browser does not support HTML5 video.
-    </video>
+video {
+    width: 100%;
+    height: auto;
+}
+
+.content {
+    background: rgba(0, 0, 0, 0.5);
+    color: #f1f1f1;
+    width: 100%;
+    padding: 60px;
+}
+</style>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="video-container">
+                <video autoplay muted loop id="myVideo">
+                    <source src="https://futurelaby.com/avatar/2023-10-28%2014-19-34.mp4">
+                    Your browser does not support HTML5 video.
+                </video>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="content">
+                <!-- Your content goes here -->
+            </div>
+        </div>
     </div>
-    """
+</div>
+"""
 st.markdown(video_html, unsafe_allow_html=True)
 
 
