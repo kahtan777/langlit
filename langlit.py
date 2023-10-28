45d2405262e3e4c293651cacb1e0bde7e90f7a02
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-28 19:34:03 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -13,11 +13,11 @@ import streamlit as st
 from streamlit_chat import message
 from utils import *
 
-keyy = st.secrets["openAI_key"]
+keyy=st.secrets["openAI_key"]
 
 st.set_page_config(layout="wide")
 
-left_column, right_column = st.columns([7, 3])
+left_column, right_column = st.columns([7,3])
 
 with left_column:
     video_html = """
@@ -30,6 +30,7 @@ with left_column:
         position: fixed; /* Fixed position to keep it in the top left corner for desktop */
         top: 5%;
         left: 3%;
+        display: none; /* Hide the video by default on larger screens */
     }
 
     @media (max-width: 768px) {
@@ -38,21 +39,23 @@ with left_column:
             top: 0;
             left: 0;
             width: 100%; /* Full width for mobile */
+            display: block; /* Display the video on smaller screens */
         }
     }
-
+    
     video {
         width: 100%;
         height: auto;
     }
-
+    
     .content {
         background: rgba(0, 0, 0, 0.5);
         color: #f1f1f1;
         width: 100%;
         padding: 20px;
+        margin-top: 50px; /* Adjust margin to make content appear below the video */
     }
-    </style>
+    </style>    
     <div class="video-container">
     <video autoplay muted loop id="myVideo">
         <source src="https://futurelaby.com/avatar/2023-10-28%2014-19-34.mp4">
@@ -61,44 +64,47 @@ with left_column:
     </div>
     """
     st.markdown(video_html, unsafe_allow_html=True)
-
+    
 with right_column:
-    empty_element = st.empty()  # Create an empty element here
-
     if 'responses' not in st.session_state:
         st.session_state['responses'] = ["How can I assist you?"]
-
+    
     if 'requests' not in st.session_state:
         st.session_state['requests'] = []
-
+    
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=keyy)
-
+    
     if 'buffer_memory' not in st.session_state:
-        st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3, return_messages=True)
-
-    system_msg_template = SystemMessagePromptTemplate.from_template(
-        template="""Answer the question as truthfully as possible using the provided context In Arabic only with now more than 50 words, 
-        and if the answer is not contained within the text below, say 'I don't know'"""
-    )
-
-    human_msg_template = HumanMessagePromptTemplate.from_template("{input}")
-
-    prompt_template = ChatPromptTemplate.from_messages(
-        [system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template]
-    )
-
+                st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+    
+    
+    system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context In Arabic only with now more than 50 words, 
+    and if the answer is not contained within the text below, say 'I don't know'""")
+    
+    
+    human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
+    
+    prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+    
     conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
-
+    
+    
+    
+    
     # container for chat history
+    
     response_container = st.container()
     # container for text box
+    
     textcontainer = st.container()
-
+    
+    
     mytext = audio.audiorec_demo_app()
-
+    
     def change_my_text_back():
-        mytext = 'default'
-
+        mytext='default'
+        
+    
     with textcontainer:
         if mytext == 'default':
             query = st.text_input("Question: ", key="input", on_change=change_my_text_back)
@@ -107,18 +113,19 @@ with right_column:
         if query:
             with st.spinner("typing..."):
                 conversation_string = get_conversation_string()
-                # st.code(conversation_string)
+                #st.code(conversation_string)
                 refined_query = query_refiner(conversation_string, query)
-                # st.subheader("Refined Query:")
-                # st.write(refined_query)
+                #st.subheader("Refined Query:")
+                #st.write(refined_query)
                 context = find_match(refined_query)
-                # print(context)
+                # print(context)  
                 response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
             st.session_state.requests.append(query)
-            st.session_state.responses.append(response)
+            st.session_state.responses.append(response) 
     with response_container:
         if st.session_state['responses']:
+    
             for i in range(len(st.session_state['responses'])):
-                message(st.session_state['responses'][i], key=str(i))
+                message(st.session_state['responses'][i],key=str(i))
                 if i < len(st.session_state['requests']):
-                    message(st.session_state["requests"][i], is_user=True, key=str(i) + '_user')
+                    message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
