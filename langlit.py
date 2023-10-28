e2f556269fa862e931ab5b061fba110213900ef7
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-28 19:16:03 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -1,3 +1,4 @@
+
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
@@ -13,12 +14,11 @@ import streamlit as st
 from streamlit_chat import message
 from utils import *
 
-keyy = st.secrets["openAI_key"]
+keyy=st.secrets["openAI_key"]
 
 st.set_page_config(layout="wide")
+
 left_column, right_column = st.columns([7,3])
-# Create Bootstrap columns
-import streamlit as st
 
 with left_column:
     video_html = """
@@ -65,7 +65,6 @@ with left_column:
 
 
 
-
 if 'responses' not in st.session_state:
     st.session_state['responses'] = ["How can I assist you?"]
 
@@ -75,33 +74,37 @@ if 'requests' not in st.session_state:
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=keyy)
 
 if 'buffer_memory' not in st.session_state:
-    st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3, return_messages=True)
+            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
 
-system_msg_template = SystemMessagePromptTemplate.from_template(
-    template="""Answer the question as truthfully as possible using the provided context In Arabic only with now more than 50 words, and if the answer is not contained within the text below, say 'I don't know'"""
-)
 
-human_msg_template = HumanMessagePromptTemplate.from_template("{input}")
+system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context In Arabic only with now more than 50 words, 
+and if the answer is not contained within the text below, say 'I don't know'""")
+
+
+human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 
 prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
 
 conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
 
-# Container for chat history
-st.markdown('<div class="container-fluid"><div class="row">', unsafe_allow_html=True)
-st.markdown('<div class="col-md-5">', unsafe_allow_html=True)
-response_container = st.container()
-st.markdown('</div>', unsafe_allow_html=True)
 
-# Container for text box
-st.markdown('<div class="col-md-7">', unsafe_allow_html=True)
-textcontainer = st.container()
-mytext = audio.audiorec_demo_app()
+
+
+# container for chat history
+with right_column:
+    response_container = st.container()
+# container for text box
+with right_column: 
+    textcontainer = st.container()
+
+with right_column:
+    mytext = audio.audiorec_demo_app()
+
 def change_my_text_back():
     mytext='default'
     
+
 with textcontainer:
-    
     if mytext == 'default':
         query = st.text_input("Question: ", key="input", on_change=change_my_text_back)
     else:
@@ -109,17 +112,19 @@ with textcontainer:
     if query:
         with st.spinner("typing..."):
             conversation_string = get_conversation_string()
+            #st.code(conversation_string)
             refined_query = query_refiner(conversation_string, query)
+            #st.subheader("Refined Query:")
+            #st.write(refined_query)
             context = find_match(refined_query)
+            # print(context)  
             response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
         st.session_state.requests.append(query)
-        st.session_state.responses.append(response)
-
-st.markdown('</div></div>', unsafe_allow_html=True)
-
+        st.session_state.responses.append(response) 
 with response_container:
     if st.session_state['responses']:
+
         for i in range(len(st.session_state['responses'])):
-            message(st.session_state['responses'][i], key=str(i))
+            message(st.session_state['responses'][i],key=str(i))
             if i < len(st.session_state['requests']):
-                message(st.session_state["requests"][i], is_user=True, key=str(i) + '_user')
+                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
