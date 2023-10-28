f9e374ce977f4095ed0bbca97f129dbdafe356db
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-28 20:25:08 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -21,58 +21,48 @@ left_column, right_column = st.columns([7,3])
 
 import streamlit as st
 
-video_html = """
-<style>
-.video-container {
-    width: 60%;
-    height: auto;
-    overflow: hidden;
-    border-radius: 0;
-    position: fixed;
-    top: 5%;
-    left: 3%;
-    z-index: 999; /* Ensure the video appears above other content */
-}
-
-@media (max-width: 768px) {
+with left_column:
+    video_html = """
+    <style>
     .video-container {
+        width: 60%;
+        height: auto;
+        overflow: hidden;
+        border-radius: 0;
         position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
+        top: 5%;
+        left: 3%;
+        z-index: 999; /* Ensure the video appears above other content */
     }
-}
 
-video {
-    width: 100%;
-    height: auto;
-}
+    @media (max-width: 768px) {
+        .video-container {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+        }
+    }
 
-.content {
-    background: rgba(0, 0, 0, 0.5);
-    color: #f1f1f1;
-    width: 100%;
-    padding: 60px;
-}
-</style>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-8">
-            <div class="video-container">
-                <video autoplay muted loop id="myVideo">
-                    <source src="https://futurelaby.com/avatar/2023-10-28%2014-19-34.mp4">
-                    Your browser does not support HTML5 video.
-                </video>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="content">
-                <!-- Your content goes here -->
-            </div>
-        </div>
+    video {
+        width: 100%;
+        height: auto;
+    }
+
+    .content {
+        background: rgba(0, 0, 0, 0.5);
+        color: #f1f1f1;
+        width: 100%;
+        padding: 60px;
+    }
+    </style>    
+    <div class="video-container floating">
+    <video autoplay muted loop id="myVideo">
+        <source src="https://futurelaby.com/avatar/2023-10-28%2014-19-34.mp4">
+        Your browser does not support HTML5 video.
+    </video>
     </div>
-</div>
-"""
+    """
 st.markdown(video_html, unsafe_allow_html=True)
 
 
@@ -105,13 +95,36 @@ with right_column:
     
     
     
-    # container for chat history
-    
+   st.markdown(
+    f"""
+    <style>
+        .reportview-container .main .block-container {{
+            width: 70%;
+        }}
+        .reportview-container .main {{
+            display: flex;
+            justify-content: space-between;
+        }}
+        .response-container {{
+            width: 45%;
+            margin-left: 15%;
+            position: relative;
+            top: 15%;
+        }}
+        .text-container {{
+            width: 45%;
+            margin-right: 15%;
+            position: relative;
+            top: 15%;
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+    with st.container():
     response_container = st.container()
-    # container for text box
-    
     textcontainer = st.container()
-    
     
     mytext = audio.audiorec_demo_app()
     
