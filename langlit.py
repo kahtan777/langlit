8ced9b99ef03cfeb6e07772466fb93b369e15e85
CakeLiar <79081062+CakeLiar@users.noreply.github.com>
2023-10-29 21:49:15 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -35,7 +35,7 @@ with left_column:
     video_html = """
     <style>
     .video-container {
-        width: 60%;
+        width: 70%;
         height: auto; 
         overflow: hidden;
         border-radius: 0;
