7a686b5e8531357e4ae2fb761eb510700c990af0
CakeLiar <79081062+CakeLiar@users.noreply.github.com>
2023-10-29 19:45:45 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -67,9 +67,8 @@ with left_column:
     </video>
     </div>
     """
-    st.markdown(video_html, unsafe_allow_html=True)
-#st.markdown(video_html, unsafe_allow_html=True)
-#components.html(video_html)
+    #st.markdown(video_html, unsafe_allow_html=True)
+    components.html(video_html)
 
 
 
