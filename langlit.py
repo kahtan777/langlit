11537ccc5c6b2269ea87f2bab5d82912e097a8f8
CakeLiar <79081062+CakeLiar@users.noreply.github.com>
2023-10-29 23:26:25 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -212,7 +212,7 @@ with right_column:
                 aud = tts.tts(response, left_column)
                 with wave.open("output.wav") as mywav:
                     duration_seconds = mywav.getnframes() / mywav.getframerate()
-                    st.write(f"Length of the WAV file: {duration_seconds:.1f} s")
+                    print(f"Length of the WAV file: {duration_seconds:.1f} s")
                     change_avatar(duration_seconds)
                 
             st.session_state.requests.append(query)
@@ -226,8 +226,6 @@ with right_column:
                     message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
 
 def play_audio(where):
-    with right_column:
-        st.write('hi')
     return
 
 
