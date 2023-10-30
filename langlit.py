80fc754e3e2630b4d6841d13039cd590aeca0c6e
kahtan777 <71929703+kahtan777@users.noreply.github.com>
2023-10-30 11:30:46 +0300

    Update langlit.py

--- langlit.py
+++ langlit.py
@@ -43,6 +43,7 @@ with left_column:
         @media (max-width: 768px) {
             .video-container {
                 position: fixed;
+                top: 0;
                 height: auto;
                 width: 40%;
                 z-index: 999; /* Ensure the video appears above other content */
